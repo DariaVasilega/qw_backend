aa5fda1fb6494aa34980c52876f404547838af75
Daria Vasilega <dariavasilega@gmail.com>
2023-09-26 14:41:37 +0300

    Resolves #11: Routing Amendments.
    - Moved Proxy controller to `api` group, and it means so that the url must start from `/api` so that be connected to the Proxy controller.
    - All Other requests trying to access the server will return 404 page.
    - Styled 404 page.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Admin Panel
+Route::get('/admin', [\App\Http\Controllers\Admin\Index::class, 'render']);
+Route::post('/admin/login', [\App\Http\Controllers\Admin\Login::class, 'action']);
+Route::post('/admin/logout', [\App\Http\Controllers\Admin\Logout::class, 'action']);
+
+// Microservices Proxy Router
+Route::any('/api/{any}', [\App\Http\Controllers\Proxy::class, 'index'])->where('any', '.*');
+
 // Common Router
 Route::view('{any}', 'errors/404', [], 404)->where('any', '.*');
 
