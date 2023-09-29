49697a74d86edc9274a8838ee478cf6bc7be7b3c
Daria Vasilega <dariavasilega@gmail.com>
2023-09-29 19:15:18 +0300

    Resolves #11: Datepicker.
    - Injected datepicker to the `app.js`.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
-import 'flowbite';
 import './nunjucks.js';
 import './htmx.js'
 import 'htmx.org/dist/ext/client-side-templates';
 import './hyperscript.js';
+import './flowbite.js';
